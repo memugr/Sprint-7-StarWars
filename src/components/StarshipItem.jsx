a42b520512ac43095405dbd1a0510a3d30985528
mei <mmullengracia@gmail.com>
2025-01-20 12:20:38 +0100

    Change general styling

--- src/components/StarshipItem.jsx
+++ src/components/StarshipItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const StarshipItem = ({ name, model }) => {
     return (
         <div className="starship-box rounded-sm p-2 w-[700px] mx-auto">
-            <h2 className="text-md mb-2">{name}</h2>
+            <h2 className="text-base mb-2">{name}</h2>
             <p>{model}</p>
         </div>
     )
