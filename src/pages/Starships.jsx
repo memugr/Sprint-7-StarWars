19085c871a3c67add71912d1f485eb001b342228
mei <mmullengracia@gmail.com>
2025-01-20 12:51:17 +0100

    Add hook useFetchStarships to call API

--- src/pages/Starships.jsx
+++ src/pages/Starships.jsx
@@ -1,32 +1,9 @@
-import { useState, useEffect } from "react";
-import StarshipItem from '../components/StarshipItem'
+import React from "react";
+import StarshipItem from "../components/StarshipItem";
+import useFetchStarships from "../hooks/useFetchStarships";
 
 const Starships = () => {
-    const [starships, setStarships] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        // Fetch the data from the API
-        const fetchStarships = async () => {
-            try {
-                const url = 'https://swapi.py4e.com/api/starships/'
-                const response = await fetch(url);
-                if (!response.ok) {
-                    throw Error('Error while fetching the data');
-                }
-                const data = await response.json();
-                setStarships(data.results);
-                setLoading(false)
-            } catch (error) {
-                setError(error.message);
-                console.error(error);
-                setLoading(false)
-            }
-        };
-
-        fetchStarships();
-    }, [])
+    const { data: starships, loading, error } = useFetchStarships("https://swapi.py4e.com/api/starships/");
 
     if (loading) {
         return (
@@ -34,15 +11,15 @@ const Starships = () => {
                 <p>Loading starships...</p>
             </div>
         )
-    }
+    };
 
     if (error) {
         return (
             <div className="flex items-center justify-center h-screen text-red-500">
                 <p>{error}</p>
             </div>
-        );
-    }
+        )
+    };
 
     return (
         <div className="flex flex-col gap-4 w-fit">
