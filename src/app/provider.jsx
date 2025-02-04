8e21a80e5817cde6478589803952aef88156e0c2
mei <mmullengracia@gmail.com>
2025-02-04 11:28:25 +0100

    Add values to the App provider

--- src/app/provider.jsx
+++ src/app/provider.jsx
@@ -1,9 +1,51 @@
+import { useState } from 'react';
 import { AppContext } from "../context/AppContext";
 
 export function AppProvider({ children }) {
+    const [state, setState] = useState({
+        user: null,
+        isAuthenticated: false,
+        email: '',
+        isLoading: false,
+    });
+
+    const value = {
+        state,
+        setState,
+        login: (user) => {
+            setState((prevState) => ({
+                ...prevState,
+                user,
+                isAuthenticated: true,
+            }));
+        },
+
+        logout: () => {
+            setState((prevState) => ({
+                ...prevState,
+                user: null,
+                isAuthenticated: false,
+            }));
+        },
+
+        setEmail: (email) => {
+            setState((prevState) => ({
+                ...prevState,
+                email,
+            }));
+        },
+        
+        setLoading: (isLoading) => {
+            setState((prevState) => ({
+                ...prevState,
+                isLoading,
+            }));
+        },
+    };
+
     return (
-        <AppContext.Provider>
+        <AppContext.Provider value={value}>
             {children}
         </AppContext.Provider>
-    )
+    );
 }
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,11 +1,11 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext } from 'react';
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 export function useAppContext() {
-    const context = useContext(AppContext)
+    const context = useContext(AppContext);
     if (!context) {
-        throw new Error('useAppContext must be used within an AppProvider')
+        throw new Error('useAppContext must be used within an AppProvider');
     }
-    return context
+    return context;
 }
