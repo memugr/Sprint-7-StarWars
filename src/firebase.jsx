f565124c7542268a40d729e5c221a3bd84515be7
mei <mmullengracia@gmail.com>
2025-02-04 11:05:40 +0100

    Move Firebase credentials in an .env file

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,23 +1,21 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDsgwjwUu8KvN6OGEITvtgVn7JOF6Cpc0k",
-    authDomain: "star-ships-89d5f.firebaseapp.com",
-    projectId: "star-ships-89d5f",
-    storageBucket: "star-ships-89d5f.firebasestorage.app",
-    messagingSenderId: "1058870699431",
-    appId: "1:1058870699431:web:a93c9e2cbab0e04abcc282",
-    measurementId: "G-VQWXYNN695"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
 // Export Firebase services
-export const auth = getAuth(app);
-export default app;
-
+export { app, analytics, auth };
